28a3021c1fadd5e2415764fbea12331f021e4e6e
Lê Nguyễn Khánh Duy <82671640+lnkhanhduy@users.noreply.github.com>
2023-05-28 19:58:39 +0700

    update path

--- src/services/GetAPI.js
+++ src/services/GetAPI.js
@@ -1,7 +1,7 @@
 export function GetAPI(path, token) {
   const options = {
     method: 'GET',
-    url: 'https://laptop-store-server-nodejs-7cwsq5ayj-lnkhanhduy.vercel.app/' + path,
+    url: 'https://laptop-store-server-nodejs-n1bo1155h-lnkhanhduy.vercel.app/' + path,
     headers: {
       'Content-Type': 'application/json',
       Authorization: 'Bearer ' + token,
